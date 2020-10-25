44198d6dc46a4c41b4bf9adb1b00935e4bcc828c
dianthy <70610913+dianthy@users.noreply.github.com>
2020-10-25 03:45:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+var mobileMenu = document.querySelector ("#mobile-menu");
+var mobileOpenButton = document.querySelector ("#open-menu-button")
+var mobileCloseButton = document.querySelector ("#close-menu-button")
+
+//Mobile menu functions
+
+function openMobileMenu() {
+	console.log("on")
+	mobileMenu.classList.add("open")
+}
+
+function closeMobileMenu() {
+	mobileMenu.classList.remove("open");
+}
+
+// Event Listeners mobile menu
+mobileOpenButton.addEventListener("click", openMobileMenu);
+mobileCloseButton.addEventListener("click", closeMobileMenu);
